2dfdb00fe98ed88a216f69863252afdefad4279a
Steve Hornsey <Steve.Hornsey@clinisys.co.uk>
2018-07-02 17:46:56 +0100

    Changed robotFactory to have clearer parameter name

--- src/factories/robotFactory.js
+++ src/factories/robotFactory.js
@@ -1,7 +1,8 @@
 const Coordinates = require('../models/coordinates')
 const Robot = require('../robot')
 
-module.exports = ({id, orientation, startNorth, startEast}) => {
+module.exports = (robotDetails) => {
+	const {id, orientation, startNorth, startEast} = robotDetails
 	var startCoordinates = new Coordinates(startNorth, startEast)
 	return new Robot(null, {id, orientation, startCoordinates})
 }
